9720bfc5cab7fd387344f87c17c3635527c53cb1
Sindre Sorhus <sindresorhus@gmail.com>
2016-09-09 21:30:26 +0700

    init

---
+++ fixture.js
@@ -0,0 +1,5 @@
+'use strict';
+const termSize = require('./');
+
+const size = termSize();
+console.log(`${size.columns}\n${size.rows}`);
---
+++ test.js
@@ -0,0 +1,17 @@
+import test from 'ava';
+import execa from 'execa';
+import m from './';
+
+test('main', t => {
+	const size = m();
+	console.log('main size:', size);
+	t.true(size.columns > 0);
+	t.true(size.rows > 0);
+});
+
+test('child', async t => {
+	const [columns, rows] = (await execa.stdout('node', ['fixture.js'])).split('\n').map(Number);
+	console.log('child size:', {columns, rows});
+	t.true(parseInt(columns, 10) > 0);
+	t.true(parseInt(rows, 10) > 0);
+});
